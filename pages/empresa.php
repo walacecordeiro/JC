42e685f366885b41db5485858e64be03a107b56f
MercinhoDS <mercinhodosantos@gmail.com>
2020-03-13 15:47:11 -0300

    pag empresa ainda em contrução + card

--- pages/empresa.php
+++ pages/empresa.php
@@ -1,6 +1,47 @@
  <section class="container" method="post" action="index.php?pag=emp">
-<h2> A Empresa </h2>
 
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum consectetur corporis doloremque perspiciatis sit hic dignissimos tenetur quod culpa, ut mollitia quaerat accusamus perferendis nostrum optio quo, adipisci beatae quis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae nulla odio accusamus cumque aperiam debitis, illo eaque nam dolorem. Natus voluptate facilis ad laudantium, aut quam corporis est cupiditate assumenda.</p>
+<div class="card mb-3">
+  <img class="card-img-top" src="img/empresa4.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title text-center">A Empresa</h2>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum consectetur corporis doloremque perspiciatis sit hic dignissimos tenetur quod culpa, ut mollitia quaerat accusamus perferendis nostrum optio quo, adipisci beatae quis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae nulla odio accusamus cumque aperiam debitis, illo eaque nam dolorem. Natus voluptate facilis ad laudantium, aut quam corporis est cupiditate assumenda.</p>
+
+    <!--<p class="card-text"><small class="text-muted"></small></p>-->
+  </div>
+</div>
+
+</section>
+
+<section class="container">
+
+<div class="row">
+
+<div class="col-md-4">
+
+<div class="card " style="width: 100%;">
+  <img class="card-img-top float-right" src="img/empresa5.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+  </div>
+
+</div>
+
+</div>
+
+<div class="col-md-8">
+
+<div class="text-center" style="width: 100%; height:100%">
+  <!--<img class="card-img-top float-right" src="img/empresa5.jpg" alt="Card image cap">-->
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+  </div>
+
+</div>
+
+</div>
 
 </section>
