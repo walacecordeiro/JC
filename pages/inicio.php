0534ae111c120acdf241e8a7cacdb6d1b514f068
walacecordeiro <walacecordeirodossantos@gmail.com>
2020-03-13 15:22:51 -0300

    conteudo inicio

--- pages/inicio.php
+++ pages/inicio.php
@@ -9,21 +9,21 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/header1.jpg" class="d-block w-100" alt="...">
+      <img src="img/header1.jpg" class="d-block w-100 seccarr" alt="...">
       <div class="carousel-caption d-sm-block">
         <h5>First slide label</h5>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="img/header2.jpg" class="d-block w-100" alt="...">
+      <img src="img/header2.jpg" class="d-block w-100 seccarr" alt="...">
       <div class="carousel-caption d-sm-block">
         <h5>Second slide label</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="img/header3.jpg" class="d-block w-100" alt="...">
+      <img src="img/header3.jpg" class="d-block w-100 seccarr" alt="...">
       <div class="carousel-caption d-sm-block">
         <h5>Third slide label</h5>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -39,4 +39,38 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+<section class="container py-5">
+    <div class="row">
+      <div class="col-sm-4 textomargin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptates autem explicabo odit, dicta vitae quae sed ut quidem modi dolor odio saepe eum cupiditate tempora iste necessitatibus provident nostrum.</div>
+      <div class="col-sm-4 textomargin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptates autem explicabo odit, dicta vitae quae sed ut quidem modi dolor odio saepe eum cupiditate tempora iste necessitatibus provident nostrum.</div>
+      <div class="col-sm-4 textomargin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptates autem explicabo odit, dicta vitae quae sed ut quidem modi dolor odio saepe eum cupiditate tempora iste necessitatibus provident nostrum.</div>
+      </div>
+
+      <div class="col-sm-12 p-0 py-5">
+
+        <div class="col-sm-12 p-0 text-center">
+          <button class="btn btn-primary btn-lg btn-block" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Leia mais</button>
+        </div>
+      
+      
+        <div class="col-sm-12 pt-4">
+          <div class="collapse multi-collapse" id="multiCollapseExample1">
+            <div class="card card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 pt-4">
+          <div class="collapse multi-collapse" id="multiCollapseExample2">
+            <div class="card card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </div>
+        </div>
+      
+    </div>
+
+</section>
+
 </section>
