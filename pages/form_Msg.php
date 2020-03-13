441cf65f47e6a31e7091538bc56bc67498a3709a
marlonsantosdesouza@gmail.com <16121742019.4@senacrjedu.local>
2020-03-13 15:13:29 -0300

    o form ta funcionando agora

--- pages/form_Msg.php
+++ pages/form_Msg.php
@@ -1,5 +1,35 @@
 <?php
-include_once('./controller/mensagens.php');
+
+include_once('mensagens.php');
+if (!empty($_POST)) {
+    $nome = trim($_POST["nome"]);
+    $email = trim($_POST["email"]);
+    $tel = trim($_POST["tel"]);
+    $cep = trim($_POST["cep"]);
+    $endereco = trim($_POST["endereco"]);
+    $numero = trim($_POST["numero"]);
+    $complemento = trim($_POST["complemento"]);
+    $mensagem = trim($_POST["mensagem"]);
+
+    $sqlcon = "insert into contato (nome, email, tel, cep, endereco, numero, complemento, msg) values ('$nome', '$email', '$tel', '$cep', '$endereco', '$numero', '$complemento', '$mensagem')";
+    
+    //Conecta o banco de dados
+    $conn = mysqli_connect(LOCAL, USER, PASS, BASE);
+    mysqli_set_charset($conn, "utf8");
+     
+    //Cadastro do Usuario
+    $salvo = mysqli_query($conn, htmlspecialchars($sqlcon)) or die(mysqli_error($conn));
+    if ($salvo){
+        //echo "<div class='alert alert-success'> Salvo </div>";
+        aviso("Salvo");
+    } else {
+        //echo "<div class='alert alert-danger'> Erro ao salvar! </div>";
+        erro("Erro ao Salvar");
+    }
+
+    mysqli_close($conn);
+}
+
 ?>
 
 <section class="container bg-branco">
@@ -19,11 +49,11 @@ include_once('./controller/mensagens.php');
         </div>
 
         <div class="form-group">
-            <input type="text" class="form-control" name="cep" maxlength="9" id="cep" onblur="pesquisacep(this.value);" required placeholder="CEP">
+            <input type="text" class="form-control" name="cep" maxlength="9" id="cep" placeholder="CEP">
         </div>
 
         <div class="form-group">
-            <input type="text" class="form-control" name="endereco" maxlength="100" id="rua" placeholder="endereço da obra">
+            <input type="text" class="form-control" name="endereco" maxlength="100" placeholder="endereço da obra">
         </div>
 
         <div class="form-group">
